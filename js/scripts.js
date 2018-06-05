7f1be091babafb45898a7fcdbbe286c33b6dde2d
Guest User <Guest@Epicodus-10E.local>
2018-06-04 20:47:27 -0700

    add backend logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,40 @@
-scripts.js
+// Business (or back-end) logic:
+
+var add = function(number1, number2) {
+  console.log(number1 + " in the add function.")
+  return number1 + number2;
+};
+//redo all the shit below this
+
+$(document).ready(function() {
+  $("form#was-validated").submit(function(event) {
+    var answer = parseInt($("input#age").val());
+    var gender = $("select#gender").val();
+
+    var quote = (100 - age) * 3;
+
+    $("#rate").empty().append(quote);
+    $("#quote").show();
+
+    event.preventDefault();
+  });
+});
+
+if (answer === 1) {
+  //show picture of the balrog
+}
+
+if (answer === 2) {
+  //show picture of the uru-khai
+}
+
+if (answer === 3) {
+  //show picture of the The Shire
+}
+
+
+
+
+
+
+// http://activelab.io/code-snippets/trigger-change-event-of-dropdown-using-jquery

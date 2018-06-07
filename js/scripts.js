747785bf9af5ea9f0c251728069b15d95d4298aa
Annie Shin <annieyshin@gmail.com>
2018-06-06 20:28:38 -0700

    remove a bunch of work because some bullshit was preventing my code from running

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,38 @@
 // Business (or back-end) logic:
-
+/*
 var add = function(number1, number2) {
   console.log(number1 + " in the add function.")
   return number1 + number2;
 };
-//redo all the shit below this
-
+*/
 $(document).ready(function() {
-  $("form#was-validated").submit(function(event) {
-    var answer = parseInt($("input#age").val());
-    var gender = $("select#gender").val();
 
-    var quote = (100 - age) * 3;
+  console.log("I am ready.");
 
-    $("#rate").empty().append(quote);
-    $("#quote").show();
-
+  $('#question1').submit(function() {
     event.preventDefault();
+    var q1 = parseInt($("#dropdown1").val());
+    console.log(q1 + ' WHERe ARE YOU');
+    // do something
   });
 });
+/*
 
 if (answer === 1) {
   //show picture of the balrog
-}
-
-if (answer === 2) {
+} else if (answer === 2) {
   //show picture of the uru-khai
-}
-
-if (answer === 3) {
+} else {
   //show picture of the The Shire
 }
 
+
+
+*/
 
 
 
 
 
 // http://activelab.io/code-snippets/trigger-change-event-of-dropdown-using-jquery
+//
